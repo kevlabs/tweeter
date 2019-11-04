13aa681d44fcf387c57982f221a89b7dbdd6a0f7
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-11-04 17:15:55 -0500

    implement char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  // recalculate length whenever key is pressed - 'input' event also works
+  $('.new-tweet textarea').on('input', function(event) {
+    const counter = $(this).siblings('.counter');
+    const charLeft = 140 - this.value.length;
+
+    counter.html(charLeft);
+    (charLeft < 0) && counter.addClass('overlimit') || counter.removeClass('overlimit');
+  });
+});
